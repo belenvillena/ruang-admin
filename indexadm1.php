b857b358933b6dd136840be4e20ccb82cdfec97f
Ana Belén Villena <villena.belen@gmail.com>
2022-04-19 02:38:17 +0200

    indexamd1 creación
    se crea el indexadm1 para mantener la session_start q no debe tenerse en el indexadm común

---
+++ indexadm1.php
@@ -0,0 +1,257 @@
+<?php 
+session_start(); 
+//--> no se inicia sesion start porqe viene por defecto desde el login
+//valido si tengo una sesion abierta y si no la tengo vuelvo al login
+if (empty($_SESSION['Usuario_Nombre']) ) {
+    header('Location: logout1.php');
+    exit;
+}
+require_once 'secciones/encabezado.php';
+
+ ?>
+
+        <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800"> Bienvenid@ <?php  echo$_SESSION['Usuario_Nombre'] ; ?> </h1>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="indexadm.php">Inicio</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Mi resumen</li>
+            </ol>
+          </div>
+
+          <div class="row mb-3">
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Productos en Tienda</div>
+                      <?php 
+                      require_once "conexionbd.php";
+                       $sql = "SELECT count(idProducto) total from producto ";
+         $result = mysqli_query($connection, $sql);
+       
+       
+        $fila = mysqli_fetch_assoc($result);
+
+
+        //if (mysqli_num_rows($contarproductos)>0) // pregunto si encontró el registro en la tabla
+        ?>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">
+          <?php echo  $fila['total'];?></div>
+        
+
+                    
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                        <span>Ultimo mes</span>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-primary"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Earnings (Annual) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Clientes Registrados</div>
+                       <?php 
+                      require_once "conexionbd.php";
+                       $sql = "SELECT count(idPersona) total from persona ";
+         $result = mysqli_query($connection, $sql);
+       
+       
+        $fila = mysqli_fetch_assoc($result);
+
+
+        //if (mysqli_num_rows($contarproductos)>0) // pregunto si encontró el registro en la tabla
+        ?>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">
+          <?php echo  $fila['total'];?></div>
+
+                      
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
+                        <span>Mes en curso</span>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-users fa-2x text-info"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- New User Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Pedidos Confirmados</div>
+                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">Consulta</div>
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 20.4%</span>
+                        <span>Mes en curso</span>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-comments fa-2x text-warning"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Pending Requests Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Pedidos Pendientes</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">Consulta</div>
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 1.10%</span>
+                        <span>Totales</span>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-shopping-cart fa-2x text-success"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Area Chart -->
+            <div class="col-xl-8 col-lg-7">
+              <div class="card mb-4">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Pedidos Generados - Mes en curso</h6>
+                  <div class="dropdown no-arrow">
+                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"
+                      aria-haspopup="true" aria-expanded="false">
+                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                      aria-labelledby="dropdownMenuLink">
+                      <div class="dropdown-header">Dropdown Header:</div>
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <div class="chart-area">
+                    <canvas id="myAreaChart"></canvas>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Pie Chart -->
+            <div class="col-xl-4 col-lg-5">
+              <div class="card mb-4">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Top Clientes - Solo modelo</h6>
+                  <div class="dropdown no-arrow">
+                    <a class="dropdown-toggle btn btn-primary btn-sm" href="#" role="button" id="dropdownMenuLink"
+                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Mes <i class="fas fa-chevron-down"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                      aria-labelledby="dropdownMenuLink">
+                      <div class="dropdown-header">Elija periodo</div>
+                      <a class="dropdown-item" href="#">Hoy</a>
+                      <a class="dropdown-item" href="#">Última semana</a>
+                      <a class="dropdown-item active" href="#">Último mes</a>
+                      <a class="dropdown-item" href="#">Último trimestre</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <div class="mb-3">
+                    <div class="small text-gray-500">Fruta Fruta
+                      <div class="small float-right"><b>600 of 800 Items</b></div>
+                    </div>
+                    <div class="progress" style="height: 12px;">
+                      <div class="progress-bar bg-warning" role="progressbar" style="width: 80%" aria-valuenow="80"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="mb-3">
+                    <div class="small text-gray-500">La fruta
+                      <div class="small float-right"><b>500 of 800 Items</b></div>
+                    </div>
+                    <div class="progress" style="height: 12px;">
+                      <div class="progress-bar bg-success" role="progressbar" style="width: 70%" aria-valuenow="70"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="mb-3">
+                    <div class="small text-gray-500">Puesto de prueba
+                      <div class="small float-right"><b>455 of 800 Items</b></div>
+                    </div>
+                    <div class="progress" style="height: 12px;">
+                      <div class="progress-bar bg-danger" role="progressbar" style="width: 55%" aria-valuenow="55"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="mb-3">
+                    <div class="small text-gray-500">Otra prueba
+                      <div class="small float-right"><b>400 of 800 Items</b></div>
+                    </div>
+                    <div class="progress" style="height: 12px;">
+                      <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="mb-3">
+                    <div class="small text-gray-500">Última prueba
+                      <div class="small float-right"><b>200 of 800 Items</b></div>
+                    </div>
+                    <div class="progress" style="height: 12px;">
+                      <div class="progress-bar bg-success" role="progressbar" style="width: 30%" aria-valuenow="30"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer text-center">
+                  <a class="m-0 small text-primary card-link" href="#">Ver mas <i
+                      class="fas fa-chevron-right"></i></a>
+                      <br>
+                </div>
+              </div>
+              <br>
+            </div>
+            <!-- Invoice Example -->
+
+
+         <?php require_once 'secciones/modallogout.php'; ?>
+
+          <?php require_once 'secciones/footer.php'; ?>
+
+ 
+  <!-- Scroll to top -->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="js/ruang-admin.min.js"></script>
+  <script src="vendor/chart.js/Chart.min.js"></script>
+  <script src="js/demo/chart-area-demo.js"></script>  
+</body>
+
+</html>
