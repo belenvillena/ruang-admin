822ac19c7eb0b302c8c47f61e1fdca41376745a5
Belen <villena.belen@gmail.com>
2022-08-22 21:38:56 -0300

    php confirmar pedido
    creo un archivo para el script de confirmacion de pedido por parte de adm

---
+++ confirmar_pedido_adm.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+//valido si tengo una sesion abierta y si no la tengo vuelvo al login
+if (empty($_SESSION['Usuario_Nombre']) ) {
+   header('Location: logout1.php');
+    exit;
+}
+
+
+?>
